5a152a758c14f27c08cab6c45e5b75b46f634c50
Yeison Yesid Diaz <yeyediso@hotmail.com>
2024-09-11 16:41:02 -0500

    mi primer presupuesto prueba

---
+++ Git.py
@@ -0,0 +1,25 @@
+# Comandos más usados en Consola:
+
+""" - ls: lista de archivos, carpeta o programas
+    - cd: change dictory cambiar de directorio o carpeta
+    - mkdir: crear un directorio o carpeta
+    - pwd: directorio o carpeta actual de trabajo
+    - cd ..: salir o cambiar del directorio o carpeta actual
+    - rm: elimina archivos
+    - rm -r: elimina carpetas 
+    - help: listado de comandos a utilizar en consola
+    - clear: limpia terminal
+    - touch: para crear archivos"""
+
+# Nota: Si el nombre tiene espacios hay que pasarlo entre comillas ya que acepta solo un nombre
+
+
+# ¿ QUE ES GIT ? : Git es una herramienta de control de versiones
+
+# Comandos de GIT
+
+"""-  Git init: inicializa mi repositorio 
+    - Git status: me dice el estado actual en mi repositorio
+    - Git add "nombre del archivo: añade un elemento al stage
+    - Git branch -M "nombre de la rama": cambia el nombre a la rama principal ej: git branch -M main
+    - Git commit -m"Que hice en la version del proyecto": envia la version del proyecto en git"""
